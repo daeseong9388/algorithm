40d82326177e383b558aa1c4edc24c6b0bc81087
daeseong9388 <ds.jeon@yonsei.ac.kr>
2022-07-09 23:42:45 +0900

    for에서 while, 공통된 조건 올려보내고 나머지 처리하기, 하나의 포맷을 원한다.

--- app/src/main/java/algorithm/array/MergeSortedArray.java
+++ app/src/main/java/algorithm/array/MergeSortedArray.java
@@ -2,16 +2,22 @@ package algorithm.array;
 
 class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int N = m + n;
         
-        int idx1 = m - 1, idx2 = n - 1;
-        for (int i = N - 1; i >= 0; i--) {
-            if (idx1 >= 0 && idx2 >= 0) {
-                if (nums1[idx1] > nums2[idx2]) { nums1[i] = nums1[idx1--]; }
-                else { nums1[i] = nums2[idx2--]; }
-            } 
-            else if (idx1 >= 0) { nums1[i] = nums1[idx1--]; }
-            else { nums1[i] = nums2[idx2--]; } 
+        int i = m-1; int j = n-1;
+        int wIdx = m + n - 1;
+        
+        while (wIdx >= 0 && i >= 0 && j >= 0) {
+            if (nums1[i] < nums2[j]) {
+                nums1[wIdx] = nums2[j];
+                j--;
+            } else {
+                nums1[wIdx] = nums1[i];
+                i--;
+            }
+            wIdx--;
         }
+        
+        for (;j>=0;j--) nums1[j] = nums2[j];
+        
     }
 }
