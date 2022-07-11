d3860ec21c0a0450581fd0df924b3f43f0023364
daeseong9388 <ds.jeon@yonsei.ac.kr>
2022-07-11 11:38:11 +0900

    hash로 풀기

--- app/src/main/java/algorithm/array/ArithmeticProgression.java
+++ app/src/main/java/algorithm/array/ArithmeticProgression.java
@@ -15,4 +15,25 @@ public class ArithmeticProgression {
         
         return true;
     }
+
+    public boolean canMakeArithmeticProgressionHash(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            set.add(num);
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        if ( (max - min) % (arr.length - 1) != 0) return false;
+        int diff = (max - min) / (arr.length - 1);
+        
+        for (int num : arr) {
+            if (num == max) continue;
+            if (!set.contains(num + diff)) return false; 
+        }
+        
+        return true;
+    }
 }
